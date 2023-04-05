9565259463af1ef9a1be5fc17c1403a23fe79e2e
aschlean <antoni@gmitruk.com>
2023-04-05 20:13:16 +0200

    added proper path and fix functions

--- 3drec/convert_npy.py
+++ 3drec/convert_npy.py
@@ -1,15 +1,22 @@
 import numpy as np
 import open3d as o3d
+import os
 
-npy_path = './experiments/exp_wild/depth'
+npy_path = os.path.dirname(__file__)+'/experiments/exp_wild/depth'
 
-def convert_npy(npy_path):
-    for i in range(0, 99):
-        print(i)
-        np_image = np.array(open3d.io.read_image(npy_path+"step_"+str(i)+".npy"))
-        print(np_image)
-        print(type(np_image))
-        o3d_image=o3d.geometry.Image(np_image)
-        print(type(o3d_image))
-        o3d.io.write_image(npy_path+"step"+str(i)+".png", o3d_image)
+def main():
+    def convert_npy(npy_path):
+        for i in range(0, 99):
+            print(i)
+            np_image = np.array(open3d.io.read_image(npy_path+"step_"+str(i)+".npy"))
+            print(np_image)
+            print(type(np_image))
+            o3d_image=o3d.geometry.Image(np_image)
+            print(type(o3d_image))
+            o3d.io.write_image(npy_path+"step"+str(i)+".png", o3d_image)
+        return
+    convert_npy(npy_path)
     return
+
+if __name__=="__main__":
+    main()
