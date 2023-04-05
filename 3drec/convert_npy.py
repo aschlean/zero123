4645fdad08d5bcc9436417cc948adbe79f5e6d94
aschlean <antoni@gmitruk.com>
2023-04-05 20:05:43 +0200

    added debug and fixed file name

--- 3drec/convert_npy.py
+++ 3drec/convert_npy.py
@@ -5,7 +5,10 @@ npy_path = './experiments/exp_wild/depth'
 
 def convert_npy(npy_path):
     for i in range(0, 99):
-        np_image = np.array(open3d.io.read_image(npy_path+"step"+str(i)+".npy"))
+        np_image = np.array(open3d.io.read_image(npy_path+"step_"+str(i)+".npy"))
+        print(np_image)
+        print(type(np_image))
         o3d_image=o3d.geometry.Image(np_image)
+        print(type(o3d_image))
         o3d.io.write_image(npy_path+"step"+str(i)+".png", o3d_image)
     return
