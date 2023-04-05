f594d79a2c8a837c98c6ca5372ed7b01d8bf0a7a
aschlean <antoni@gmitruk.com>
2023-04-05 20:57:42 +0200

    added parsed arguments

--- 3drec/convert_npy.py
+++ 3drec/convert_npy.py
@@ -1,20 +1,29 @@
 import numpy as np
 import open3d as o3d
 import os
+import argparse
+import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--steps", type=int, help="number of depth images to convert",
+                    action="store_true")
+args = parser.parse_args()
 npy_path = os.path.dirname(__file__)+'/experiments/exp_wild/depth/'
 
+
 def main():
-    def convert_npy(npy_path):
-        for i in range(0, 99):
+    def convert_npy(npy_path, steps):
+        np.set_printoptions(threshold=sys.maxsize)
+        for i in range(0, steps):
             print(i)
             np_image = np.load(npy_path+"step_"+str(i)+".npy")
             print(np_image.shape, np_image.dtype)
+            print(np_image)
             o3d_image=o3d.geometry.Image(np_image)
             print(type(o3d_image))
-            o3d.io.write_image(npy_path+"step"+str(i)+".png", o3d_image)
+            o3d.io.write_image(npy_path+"step_"+str(i)+".png", o3d_image)
         return
-    convert_npy(npy_path)
+    convert_npy(npy_path, args.steps)
     return
 
 if __name__=="__main__":
