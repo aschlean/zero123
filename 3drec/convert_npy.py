4e4a37e9d162fae3fc46d6f8fc59af9fa29b3d22
aschlean <antoni@gmitruk.com>
2023-04-05 20:18:16 +0200

    changed npy file open method

--- 3drec/convert_npy.py
+++ 3drec/convert_npy.py
@@ -8,9 +8,8 @@ def main():
     def convert_npy(npy_path):
         for i in range(0, 99):
             print(i)
-            np_image = np.array(o3d.io.read_image(npy_path+"step_"+str(i)+".npy"))
-            print(np_image)
-            print(type(np_image))
+            np_image = np.array(open(npy_path+"step_"+str(i)+".npy"))
+            print(np_image.shape, np_image.dtype)
             o3d_image=o3d.geometry.Image(np_image)
             print(type(o3d_image))
             o3d.io.write_image(npy_path+"step"+str(i)+".png", o3d_image)
